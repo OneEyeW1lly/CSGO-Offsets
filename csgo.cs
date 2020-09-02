f814f797289154c90e1c8ffa5266bee7971e4e5c
JokinAce <pisferko@gmail.com>
2020-09-02 13:31:56 +0000

    ✨ CS:GO Offsets Update 2020-09-02

--- csgo.cs
+++ csgo.cs
@@ -1,6 +1,6 @@
 using System;
 
-// 2020-08-28 10:54:59.307067500 UTC
+// 2020-09-02 13:31:51.496448 UTC
 
 namespace hazedumper
 {
@@ -139,7 +139,7 @@ namespace hazedumper
         public const Int32 dwZoomSensitivityRatioPtr = 0xD4A700;
         public const Int32 dwbSendPackets = 0xD418A;
         public const Int32 dwppDirect3DDevice9 = 0xA7030;
-        public const Int32 find_hud_element = 0x35064F10;
+        public const Int32 find_hud_element = 0x30E14F10;
         public const Int32 force_update_spectator_glow = 0x3A3E82;
         public const Int32 interface_engine_cvar = 0x3E9EC;
         public const Int32 is_c4_owner = 0x3B0910;
