54e7ee83318a99e8b3915e128ceedc5bedc2c593
JokinAce <jokinace@pornhubstaff.com>
2021-06-26 00:19:19 +0200

    ✨ CS:GO Offsets Update 2021-06-26

--- csgo.vb
+++ csgo.vb
@@ -1,4 +1,4 @@
-' 2021-06-21 05:10:57.644205100 UTC
+' 2021-06-25 22:19:16.667819300 UTC
 
 Namespace hazedumper
     Public Shared Class netvars
@@ -141,7 +141,7 @@ Namespace hazedumper
         Public Const dwZoomSensitivityRatioPtr as Integer = &HD93D18
         Public Const dwbSendPackets as Integer = &HD76DA
         Public Const dwppDirect3DDevice9 as Integer = &HA7050
-        Public Const find_hud_element as Integer = &H3075F980
+        Public Const find_hud_element as Integer = &H2B19F980
         Public Const force_update_spectator_glow as Integer = &H3AFECA
         Public Const interface_engine_cvar as Integer = &H3E9EC
         Public Const is_c4_owner as Integer = &H3BCB10
