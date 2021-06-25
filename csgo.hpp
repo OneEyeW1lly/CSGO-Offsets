5c0b0505d5d79dd80d10b96ae0373742cdd2af16
JokinAce <jokinace@pornhubstaff.com>
2021-06-26 00:19:17 +0200

    ✨ CS:GO Offsets Update 2021-06-26

--- csgo.hpp
+++ csgo.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <cstddef>
 
-// 2021-06-21 05:10:57.644205100 UTC
+// 2021-06-25 22:19:16.667819300 UTC
 
 namespace hazedumper {
 namespace netvars {
@@ -144,7 +144,7 @@ constexpr ::std::ptrdiff_t dwYawPtr = 0xD8EA78;
 constexpr ::std::ptrdiff_t dwZoomSensitivityRatioPtr = 0xD93D18;
 constexpr ::std::ptrdiff_t dwbSendPackets = 0xD76DA;
 constexpr ::std::ptrdiff_t dwppDirect3DDevice9 = 0xA7050;
-constexpr ::std::ptrdiff_t find_hud_element = 0x3075F980;
+constexpr ::std::ptrdiff_t find_hud_element = 0x2B19F980;
 constexpr ::std::ptrdiff_t force_update_spectator_glow = 0x3AFECA;
 constexpr ::std::ptrdiff_t interface_engine_cvar = 0x3E9EC;
 constexpr ::std::ptrdiff_t is_c4_owner = 0x3BCB10;
